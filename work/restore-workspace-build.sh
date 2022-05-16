641792b44100ae0a8db7f5ff607091af73dcd806
Vasilii Pochkaenko <vassili.pochkaenko@vitec.com>
2022-05-16 09:41:52 +0700

    feat: use the domaine name instead the ip addres for the ep builds storage

--- work/restore-workspace-build.sh
+++ work/restore-workspace-build.sh
@@ -4,7 +4,7 @@ i3-msg "workspace 1 build; append_layout ~/.config/i3/work/workspace-build.json"
 
 firefox --new-window http://ep-build-server.vikra:8080/job/VK_EP_unittests#main-panel &
 firefox --new-window http://ep-build-server.vikra:8080/job/VK_EP4/ &
-firefox --new-window http://192.168.216.89:8081/VIKRA/8.2.0.2040/EP4/ &
+firefox --new-window http://ep-builds.vikra:8081/VIKRA/8.2.0.2040/EP4/ &
 firefox --new-window http://ep-build-server.vikra:8080/job/VK_EP5/ &
-firefox --new-window http://192.168.216.89:8081/VIKRA_EP5/8.2.0.857/ &
+firefox --new-window http://ep-builds.vikra:8081/VIKRA_EP5/8.2.0.857/ &
 
